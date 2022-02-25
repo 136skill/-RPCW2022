5c77a46a7c856d418d155af7fdaadab23e94c3aa
136Skill <beatriz.lacerdarlive.com.pt>
2022-02-25 15:23:39 +0000

    atores

--- TPC2/geradorHTML.py
+++ TPC2/geradorHTML.py
@@ -1,5 +1,6 @@
 import json
 
+#páginas filmes
 filmes = json.load(open("./cinemaATP.json",encoding="utf-8"))
 
 i=1
@@ -36,7 +37,9 @@ for item in filmes:
 </html>''')
     c += 1
 
-t = open("./html/index.html", "w")
+
+#Index
+t = open("./html/index.html", "w",encoding="utf-8")
 i = 1
 
 t.write(f'''<!DOCTYPE html>\n
@@ -56,6 +59,71 @@ for titulo in filmes:
 t.write('''</div>
 </body>
 </html>''')
+
+
+#Atores
+dic = {}
+i = 1
+for obj in filmes:
+    for at in obj['cast']:
+        dic.setdefault(at, []).append(obj['title'])
+
+sort_keys = dic.items()
+new_items = sorted(sort_keys)
+
+
+i = 1
+c = 1
+for ator in dic:
+    f = open("./htmlAtores/a" + str(i) + ".html", "w",encoding="utf-8")
+    i += 1
+    f.write(f'''<!DOCTYPE html>\n
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>{ator}</title>
+
+</head>
+<body>
+    <div class="w3-container w3-margin-left">''')
+    f.write(f'<p><b>Ator:{ator}</b></p>')
+    f.write('<p><b>Filmes em que participa:</b></p>')
+    f.write('<ol>')
+    for cas in dic[ator]:
+        f.write('<li>'+ cas +'</li>')
+    f.write('</ol>')
+    f.write('</div>')
+    f.write('''</body>
+</html>''')
+
+#Lista Atores
+t = open("./htmlAtores/atores.html", "w",encoding="utf-8")
+i = 1
+
+t.write(f'''<!DOCTYPE html>\n
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title> Filmes </title>
+</head>
+<body>
+<div class="w3-container w3-margin-left">''')
+
+for ator in dic:
+    t.write(f'''<p><a href=./atores/a{i}> {ator} </a></p>''')
+    i += 1
+
+t.write('''</div>
+</body>
+</html>''')
+
+
+
+    
+
+
 
 
 
--- TPC2/servidor.js
+++ TPC2/servidor.js
@@ -4,6 +4,8 @@ var url = require('url')
 
 http.createServer(function(req,res) {
     var myurl = req.url.substring(9)
+    console.log(myurl)
+    console.log(req.url)
     var m = url.parse(req.url, true).pathname
     if(m == '/filmes'){
         fs.readFile("./html/index.html", function(err,data){
@@ -18,6 +20,30 @@ http.createServer(function(req,res) {
             })
          
         }
+    else if(m == '/atores'){
+        fs.readFile("./htmlAtores/atores.html", function(err,data){
+            res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+            if(err){
+                res.write("<p> Erro na leitura do ficheiro </p>")
+            }
+            else{
+                res.write(data)
+            }
+            res.end()
+            })
+    }
+    else if (m.includes('/atores/a')){
+        fs.readFile("./htmlAtores/a" + myurl + ".html", function(err,data){
+            res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+            if(err){
+                res.write("<p> Erro na leitura do ficheiro </p>")
+            }
+            else{
+                res.write(data)
+                }
+            res.end()
+            })
+    }
     else {
         fs.readFile("./html/f" + myurl + ".html", function(err,data){
         res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
